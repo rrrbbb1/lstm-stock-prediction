9a7d377b2cb571652ebb0e1f1b81a1c334798d07
Jimmy-Bao-01 <jimmybao2001@gmail.com>
2024-12-19 14:13:05 +0100

    jimmy

---
+++ plot.py
@@ -0,0 +1,154 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from metrics import sharpe_ratio
+
+plt.style.use('seaborn-v0_8-whitegrid')
+
+
+def set_axis(ax=None, figsize=(8, 5), title=None, fig=None):
+    if ax is None:
+        fig, ax = plt.subplots(1, 1, figsize=figsize)
+    if title is not None:
+        ax.set_title(title)
+    return fig, ax
+
+
+def line(
+    df,
+    sort=True,
+    figsize=(8, 5),
+    ax=None,
+    title="",
+    cumsum=False,
+    loc="center left",
+    bbox_to_anchor=(1, 0.5),
+    legend_sharpe_ratio=None,
+    legend=True,
+    yscale=None,
+    start_date=None,
+):
+    df = df.copy()
+    if loc == "best":
+        bbox_to_anchor = None
+    if isinstance(df, dict) | isinstance(df, list):
+        df = pd.concat(df, axis=1)
+    if isinstance(df, pd.Series):
+        df = df.to_frame()
+    if start_date is not None:
+        df = df[start_date:]
+    if cumsum & (legend_sharpe_ratio is None):
+        legend_sharpe_ratio = True
+    if legend_sharpe_ratio:
+        df.columns = [f"{c}: sr={sharpe_ratio(df[c]): 3.2f}" for c in df.columns]
+    if cumsum:
+        df = df.cumsum()
+    if sort:
+        df = df.loc[:, lambda x: x.iloc[-1].sort_values(ascending=False).index]
+    fig, ax = set_axis(ax=ax, figsize=figsize, title=title)
+    ax.plot(df.index, df.values)
+    if legend:
+        ax.legend(df.columns, loc=loc, bbox_to_anchor=bbox_to_anchor)
+    if yscale == "log":
+        ax.set_yscale("log")
+
+
+def bar(
+    df,
+    err=None,
+    sort=True,
+    figsize=(8, 5),
+    ax=None,
+    title=None,
+    horizontal=False,
+    baseline=None,
+    rotation=0,
+):
+    if isinstance(df, pd.DataFrame):
+        df = df.squeeze()
+    if isinstance(df, dict):
+        df = pd.Series(df)
+    if sort:
+        df = df.sort_values()
+    if err is not None:
+        err = err.loc[df.index]
+    labels = df.index
+    x = np.arange(len(labels))
+    fig, ax = set_axis(ax=ax, figsize=figsize, title=title)
+    if horizontal:
+        ax.barh(x, df.values, xerr=err, capsize=5)
+        ax.set_yticks(x)
+        ax.set_yticklabels(labels, rotation=0)
+        if baseline in df.index:
+            df_ = df.copy()
+            df_[df.index != baseline] = 0
+            ax.barh(x, df_.values, color="lightgreen")
+    else:
+        ax.bar(x, df.values, yerr=err, capsize=5)
+        ax.set_xticks(x)
+        ax.set_xticklabels(labels, rotation=0)
+        if baseline in df.index:
+            df_ = df.copy()
+            df_[df.index != baseline] = 0
+            ax.bar(x, df_.values, color="lightgreen")
+    ax.set_title(title)
+
+
+def heatmap(
+    df,
+    ax=None,
+    fig=None, 
+    figsize=(8, 5),
+    title=None,
+    vmin=None,
+    vmax=None,
+    vcompute=True,
+    cmap="RdBu",
+):
+    labels_x = df.index
+    x = np.arange(len(labels_x))
+    labels_y = df.columns
+    y = np.arange(len(labels_y))
+    if vcompute:
+        vmax = df.abs().max().max()
+        vmin = -vmax
+    fig, ax = set_axis(ax=ax, figsize=figsize, title=title, fig=fig)
+    pos = ax.imshow(
+        df.T.values, cmap=cmap, interpolation="nearest", vmax=vmax, vmin=vmin
+    )
+    ax.set_xticks(x)
+    ax.set_yticks(y)
+    ax.set_xticklabels(labels_x, rotation=90)
+    ax.set_yticklabels(labels_y)
+    ax.grid(True)
+    fig.colorbar(pos, ax=ax)
+
+
+def scatter(
+    df,
+    ax=None,
+    xscale=None,
+    yscale=None,
+    xlabel=None,
+    ylabel=None,
+    xticks=None,
+    yticks=None,
+    figsize=(8, 5),
+    title=None,
+):
+    fig, ax = set_axis(ax=ax, figsize=figsize, title=title)
+    ax.scatter(df, df.index, facecolors="none", edgecolors="b", s=50)
+    if xlabel is not None:
+        ax.set_xlabel(xlabel)
+    if ylabel is not None:
+        ax.set_ylabel(ylabel)
+    if xscale is not None:
+        ax.set_xscale(xscale)
+    if yscale is not None:
+        ax.set_yscale(yscale)
+    if yticks is not None:
+        ax.set_yticks(yticks)
+        ax.set_yticklabels(yticks)
+    if xticks is not None:
+        ax.set_xticks(xticks)
+        ax.set_xticklabels(xticks)
